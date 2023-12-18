04a322c0761e0991c0ba7488cf1509c7520b192a
HannaMK3 <62847093+HannaMK3@users.noreply.github.com>
2023-12-18 12:57:51 +0200

    Add files via upload

---
+++ Code/Fig4_PsychometricCurveMetrics.m
@@ -0,0 +1,34 @@
+%% This code creates Fig. 4, which describes the metrics computed from the 
+% psychometric curves
+%% Blue curves
+%%
+PsychometricData_F=85-[140:-10:30]';
+PsychometricData_F(:,2)=[0; 0; 0; 1; 3; 3; 7; 6; 5; 7; 8; 6];
+PsychometricData_F(:,3)=8;
+PsychometricData_SS=85-[140:-10:30]';
+PsychometricData_SS(:,2)=[0; 0; 0; 0; 0; 1; 0; 0; 3; 5; 4; 6];
+PsychometricData_SS(:,3)=8;
+
+[PSE1,PSE2]=PsychometricFitting_ForMetricDescription(PsychometricData_F,PsychometricData_SS, 'Real');
+set(gca, 'fontname', 'Times New Roman')
+set(gca, 'fontsize', 10)
+
+fig=gcf;
+fig.Position=[0, 0, 250, 210];
+
+%% Red curves
+%%
+PsychometricData_F=85-[140:-10:30]';
+PsychometricData_F(:,2)=[0; 0; 1; 2; 2; 4; 6; 8; 7; 8; 7; 8];
+PsychometricData_F(:,3)=8;
+PsychometricData_SS=85-[140:-10:30]';
+PsychometricData_SS(:,2)=[0; 0; 0; 1; 2; 1; 2; 4; 6; 4; 6; 6];
+PsychometricData_SS(:,3)=8;
+
+
+[PSE1,PSE2]=PsychometricFitting_ForMetricDescription(PsychometricData_F,PsychometricData_SS, 'Predicted');
+set(gca, 'fontname', 'Times New Roman')
+set(gca, 'fontsize', 10)
+
+fig=gcf;
+fig.Position=[0, 0, 250, 210];
