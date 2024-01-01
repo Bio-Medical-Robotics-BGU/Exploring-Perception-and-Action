7e18e27aaaa64cd7930739cd85f2680e14cfa433
HannaMK3 <62847093+HannaMK3@users.noreply.github.com>
2024-01-01 11:18:26 +0200

    Update Create10Folds.py

--- Code/Network-Predictions/Create10Folds.py
+++ Code/Network-Predictions/Create10Folds.py
@@ -9,10 +9,10 @@ from scipy.io import savemat
 
 # %% Paths
 # Replace the following directory with the location of the saved preprocessed data
-MatPath = r"C:\Users\hanna\OneDrive\MATLAB\lab\PhD\Perception_and_GF_prediction\DL_perception\OnlyInSurface\ParticipantMatsAndVecs\final"
+MatPath = r"D:\OneDrive\PerceptionAction\Preprocessed"
 
-# Replace the following directory with the location to which to save the fold indices
-ProjectPath = r"C:\Users\hanna\OneDrive\lab\PhD\python\PerceptionActionInSurface\Final"
+# Replace the following directory with the location of the saved fold indices, and to which the datasets will be saved
+ProjectPath = r"D:\OneDrive\PerceptionAction\Data"
 DatasetPath = os.path.join(ProjectPath, 'DatasetsFolds')
 
 k = 10
