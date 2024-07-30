5213b35859e7321488bc171d12546ca2165d65f2
HannaMK3 <62847093+HannaMK3@users.noreply.github.com>
2024-07-30 09:53:35 +0300

    Update Fig4_PsychometricCurveMetrics.m

--- Code/Analyses/Fig4_PsychometricCurveMetrics.m
+++ Code/Analyses/Fig4_PsychometricCurveMetrics.m
@@ -22,10 +22,9 @@ fig.Position=[0, 0, 250, 210];
 %% Red curves
 %%
 PsychometricData_F=85-[140:-10:30]';
-PsychometricData_F(:,2)=[0; 0; 0; 1; 1; 2; 6; 6; 7; 8; 8; 8];
+PsychometricData_F(:,2)=[0; 0; 2; 5; 5; 5; 6; 6; 7; 8; 8; 8];
 PsychometricData_F(:,3)=8;
 PsychometricData_SS=85-[140:-10:30]';
-% PsychometricData_SS(:,2)=[0; 0; 0; 1; 2; 1; 2; 4; 6; 4; 6; 6];
 PsychometricData_SS(:,2)=[0; 0; 0; 2; 2; 1; 3; 5; 7; 5; 6; 5];
 PsychometricData_SS(:,3)=8;
 
